eba57914f26e55b6ba2fa109c95a5001e168e373
Tsjippy <ewaldha@hotmail.com>
2024-10-04 12:57:22 +0100

    changed: updated deps

--- blocks/mailchimp_options/build/index.asset.php
+++ blocks/mailchimp_options/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element'), 'version' => '69b94471f3dcedee07b7');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post'), 'version' => '8e9e104678c9af254376');
