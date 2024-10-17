57a70799ca2f9009db12f2220d91f3a93fabe003
Tsjippy <ewaldha@hotmail.com>
2024-10-17 09:52:07 +0100

    updated: readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A module for the sim-plugin to add signal messaging
+This module adds the possibility to send e-mails via Mailchimp. <br>
+Post or page contents can be send as e-mail upon publishing or updating.<br>
+Create your api key for Mailchimp <a href='https://mailchimp.com/developer/marketing/guides/quick-start/'>here</a>.<br>
--- php/__module_menu.php
+++ php/__module_menu.php
@@ -8,24 +8,6 @@ DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
 DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
 
-add_filter('sim_submenu_description', function($description, $moduleSlug){
-	//module slug should be the same as the constant
-	if($moduleSlug != MODULE_SLUG)	{
-		return $description;
-	}
-
-	ob_start();
-	?>
-	<p>
-		This module adds the possibility to send e-mails via Mailchimp. <br>
-		Post or page contents can be send as e-mail upon publishing or updating.<br>
-		Create your api key for Mailchimp <a href='https://mailchimp.com/developer/marketing/guides/quick-start/'>here</a>.<br>
-	</p>
-	<?php
-
-	return ob_get_clean();
-}, 10, 2);
-
 add_filter('sim_submenu_options', function($optionsHtml, $moduleSlug, $settings){
 	//module slug should be the same as grandparent folder name
 	if($moduleSlug != MODULE_SLUG){
