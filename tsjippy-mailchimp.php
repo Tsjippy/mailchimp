53a87b7712bea4687fb52bd39323d160556dccc8
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:25:44 +0200

    skip:changed: removed 'tested up to'

--- tsjippy-mailchimp.php
+++ tsjippy-mailchimp.php
@@ -10,10 +10,9 @@ namespace TSJIPPY\MAILCHIMP;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
- * Plugin URI:            https://github.com/Tsjippy/mailchimp
+ * Plugin URI:           https://github.com/Tsjippy/mailchimp
  * Tested:               7.1
- * TextDomain:            tsjippy
+ * TextDomain:           tsjippy
  * Requires Plugins:    
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
