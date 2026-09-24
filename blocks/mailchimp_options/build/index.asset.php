dee83152bdb6f6483c53135f4ec2ca2d1f88a766
Tsjippy <ewaldha@hotmail.com>
2026-09-24 07:35:39 +0200

    skip:

--- blocks/mailchimp_options/build/index.asset.php
+++ blocks/mailchimp_options/build/index.asset.php
@@ -1,1 +1,15 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '6f0a345a1c5338964a7b');
+<?php return array(
+	'dependencies' => array(
+		'react-jsx-runtime',
+		'wp-api-fetch',
+		'wp-components',
+		'wp-core-data',
+		'wp-data',
+		'wp-editor',
+		'wp-element',
+		'wp-i18n',
+		'wp-plugins',
+		'wp-url'
+	),
+	'version' => '6f0a345a1c5338964a7b'
+);
